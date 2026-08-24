b1386226803e63a2f17168f7814ecdd51b1f63a4
Venyl <zexe2121@gmail.com>
2026-08-25 01:40:17 +0200

    change path

--- vite.config.ts
+++ vite.config.ts
@@ -15,13 +15,7 @@ export default defineConfig({
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 
-			adapter: adapter({}),
-			output: {
-				bundleStrategy: "single"
-			},
-			paths: {
-				base: process.argv.includes("dev") ? "" : "/counterlock"
-			}
+			adapter: adapter({})
 		})
 	]
 });
