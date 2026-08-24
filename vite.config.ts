1c287c6f2b479ecfed6bcca19832faae7fc3cc66
Venyl <zexe2121@gmail.com>
2026-08-25 01:28:15 +0200

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter({}),
 			paths: {
-				base: process.argv.includes("dev") ? "" : (process.env.BASE_PATH as `/${string}`)
+				base: process.argv.includes("dev") ? "" : "/counterlock"
 			}
 		})
 	]
